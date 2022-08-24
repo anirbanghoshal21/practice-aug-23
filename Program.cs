4e2ceb375e8c09dc5a5d69c3db334c207b687c46
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-24 19:24:53 +0530

    problem 3

--- Program.cs
+++ Program.cs
@@ -1,31 +1,101 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace problem_1
+
+struct employee
 {
-    internal class Program
+    public int empno;
+    public string ename;
+    public double sal;
+    public int deptno;
+};
+
+public class Structure
+{
+
+    public static void Main(string[] args)
     {
+        employee employees1;
+
+        employees1.empno = 1;
+        employees1.ename = "Ashok Gupta";
+        employees1.sal = 4322;
+        employees1.deptno = 64;
 
 
-        public static int Sum(int num1, int num2)
+        Console.WriteLine("employees 1 empno : {0}", employees1.empno);
+        Console.WriteLine("employees 1 ename : {0}", employees1.ename);
+        Console.WriteLine("employees 1 sal : {0}", employees1.sal);
+        Console.WriteLine("employees 1 deptno :{0}", employees1.deptno); 
+    }
+
+        public int empno
+    {
+        get
         {
-            int total;
-            total = num1 + num2;
-            return total;
+            return empno;
         }
-
-        public static void Main()
-            {
-                Console.Write("Enter two numbers: ");
-                int number1 = Convert.ToInt32(Console.ReadLine());
-                int number2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("\nThe sum of two numbers is : {0} \n", Sum(number1, number2));
-
-                Console.ReadLine();
-            }
+        set
+        {
+            empno = value;
         }
-    } 
+    }
 
+    public string ename
+    {
+        get
+        {
+            return ename;
+        }
+        set
+        {
+            ename = value;
+        }
+    }
+
+    public double sal
+    {
+        get
+        {
+            return sal;
+        }
+        set
+        {
+            sal = value;
+        }
+    }
+
+    public int deptno
+    {
+        get
+        {
+            return deptno;
+        }
+        set
+        {
+            deptno = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        return "empno = " + empno + ", ename = " + ename + ", sal = " + sal; 
+    }
+
+}
+class ExampleDemo
+{
+    public static void Main()
+    {
+        employee e = new employee();
+
+        e.deptno = 1;
+        e.ename = "Ashok Gupta";
+        e.sal = 4322;
+        e.deptno = 64;
+        Console.WriteLine("Employee Info: {0}", e);
+
+
+        Console.ReadKey();
+    }
+}
