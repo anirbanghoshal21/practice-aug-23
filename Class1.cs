03bde31ecc0fa2338330e92dbd45fd9d5b37b344
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-24 20:22:38 +0530

    problem 6

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem6
+{
+    enum deptnames
+    {
+        Purchase=1,
+        Sales=2,
+        Training=3,
+        Accounts=4,
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,28 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace problem_1
+namespace problem6
 {
     internal class Program
     {
+      
 
-
-        public static int Sum(int num1, int num2)
+        static void Main(string[] args)
         {
-            int total;
-            total = num1 + num2;
-            return total;
+            //Console.WriteLine(deptnames.Sales);
+
+            int num = (int)deptnames.Purchase;
+            Console.WriteLine(num);
+
+            Console.WriteLine("display deptnos");
+            string deptname = Convert.ToString(num);
+
+           
+            Console.WriteLine(nameofdeptno);
+
+
+
+            Console.ReadLine();
         }
-
-        public static void Main()
-            {
-                Console.Write("Enter two numbers: ");
-                int number1 = Convert.ToInt32(Console.ReadLine());
-                int number2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("\nThe sum of two numbers is : {0} \n", Sum(number1, number2));
-
-                Console.ReadLine();
-            }
-        }
-    } 
-
+    }
+}
