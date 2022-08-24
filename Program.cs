6089ecc96a3718c200307ad2d4d0f80effc7956f
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-24 20:15:50 +0530

    problem 8

--- Program.cs
+++ Program.cs
@@ -4,28 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace problem_1
+namespace problem8
 {
     internal class Program
     {
-
-
-        public static int Sum(int num1, int num2)
+        static void Main(string[] args)
         {
-            int total;
-            total = num1 + num2;
-            return total;
-        }
+            int a = 10;
+            int b = 0;
+            int c = 0;
 
-        public static void Main()
+            try
             {
-                Console.Write("Enter two numbers: ");
-                int number1 = Convert.ToInt32(Console.ReadLine());
-                int number2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("\nThe sum of two numbers is : {0} \n", Sum(number1, number2));
-
-                Console.ReadLine();
+                c = a / b;
+                Console.WriteLine(c);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.ReadLine();   
+            }
+            finally
+            {
+                Console.Write("Re-try with a different number.");
             }
         }
-    } 
-
+    }
+}
