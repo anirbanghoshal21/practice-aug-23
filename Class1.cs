865ad62d26176f3bb0d7a382256c6b14ea139f69
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-24 19:32:32 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem4
+{
+    internal class Class1
+    {
+        struct employee
+        {
+        public int empno;
+        public string ename;
+        public double sal;
+        public int deptno;
+    };
+}
+}
